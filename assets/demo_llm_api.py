afaaa5ef85a2156188db79620e9e03ac9e18c467
finkene <nfinkel@us.ibm.com>
2024-04-26 15:08:23 -0400

    Add files via upload

--- assets/demo_llm_api.py
+++ assets/demo_llm_api.py
@@ -36,9 +36,11 @@ from ibm_cloud_sdk_core import IAMTokenManager
 url = "https://us-south.ml.cloud.ibm.com"
 
 # These global variables will be updated in get_credentials() functions
-watsonx_project_id = "YOUR PROJECT ID"  ##EDIT THIS LINE
+#watsonx_project_id = "YOUR PROJECT ID"  ##EDIT THIS LINE
+watsonx_project_id = "a350a46a-ed2e-458f-8b08-ab5c4aa8c4bf"
 # Replace with your IBM Cloud key
-api_key = "YOUR APIKEY"  ##EDIT THIS LINE
+#api_key = "YOUR APIKEY"  ##EDIT THIS LINE
+api_key ="0FEL2nLQUrrJhApJ87KstSe79VwRov-Su27YTXbTQeiO"
 
 def get_credentials():
 
